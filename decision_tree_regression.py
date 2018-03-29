dbf633722c44691e10402561c35a9d5211985ad2
nwhoppe <nwhoppe@wisc.edu>
2018-03-29 14:13:45 -0500

    added output to make labeled graph via dot

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 import argparse
+import itertools
 import numpy as np
 import sys
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 from linear_regression_cv import cross_validation_dict_and_combos_from_txt, data_arrays_from_cv_dict, \
     plot_predicted_actual
 
 
 def descision_tree_regression(cross_validation_files, criterion):
+    base_name = cross_validation_files[0].split('/')[-1].split('_cv_')[0]
+    feature_names = []
+    nucleotides = 'ACGT'
+    positions = range(0, 19)
+    single_position_items = list(itertools.product(positions, nucleotides))
+    pairwise_positions = list(itertools.combinations(positions, 2))
+    pairwise_nucleotides = list(itertools.product(nucleotides, nucleotides))
+    pairwise_items = list(itertools.product(pairwise_positions, pairwise_nucleotides))
+    for position, nucleotide in single_position_items:
+        feature_names.append('{0}{1}'.format(position, nucleotide))
+    for position_pair, nucleotide_pair in pairwise_items:
+        feature_names.append('{0}{1} {2}{3}'.format(
+            position_pair[0], nucleotide_pair[0], position_pair[1], nucleotide_pair[1]))
     k = len(cross_validation_files)
     cross_validation_dict, cross_validation_combos = \
         cross_validation_dict_and_combos_from_txt(cross_validation_files)
@@ -18,7 +32,7 @@ def descision_tree_regression(cross_validation_files, criterion):
 
     for cv_combo in cross_validation_combos:
         x_train, rep_train, ind_train, fi_train = data_arrays_from_cv_dict(cv_combo, cross_validation_dict)
-        missing_set = list(set(range(k)) - set(cv_combo))[0]
+        missing_set = list(set(range(k)) - set(cv_combo))
         x_valid, rep_valid, ind_valid, fi_valid = data_arrays_from_cv_dict(missing_set, cross_validation_dict)
 
         training_dict = {
@@ -41,13 +55,16 @@ def descision_tree_regression(cross_validation_files, criterion):
             regressor.fit(x_train, training_dict[variable])
             y_hat = regressor.predict(x_valid)
             r_squared = regressor.score(x_valid, valid_dict[variable])
+            r_squared_dict[variable].append(r_squared)
+            export_graphviz(regressor, feature_names=feature_names,
+                            out_file='{0}_{1}_{2}.dot'.format(base_name,variable, missing_set[0]))
             if variable == 'both':
                 validation_dict[variable]['rep'].extend(valid_dict[variable][:, 0])
                 validation_dict[variable]['ind'].extend(valid_dict[variable][:, 1])
                 prediction_dict[variable]['rep'].extend(y_hat[:, 0])
                 prediction_dict[variable]['ind'].extend(y_hat[:, 1])
             else:
-                validation_dict[variable].extend(validation_dict[variable])
+                validation_dict[variable].extend(valid_dict[variable])
                 prediction_dict[variable].extend(y_hat)
             sys.stdout.write('{0}: {1}\t'.format(variable, r_squared))
         sys.stdout.write('\n')
